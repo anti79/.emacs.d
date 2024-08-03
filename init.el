2120844659e7fdf588c663aba980fd7e4850f4ac
daniel <@noemail>
2024-08-03 12:36:19 +0300

    python

--- init.el
+++ init.el
@@ -22,7 +22,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(adwaita))
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages '(elpy))
  '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -31,3 +32,15 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'set-goal-column 'disabled nil)
+
+
+;;python
+(add-hook 'python-mode-hook
+	    (lambda ()
+		    (setq-default indent-tabs-mode t)
+		    (setq-default py-indent-tabs-mode t)))
+
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
