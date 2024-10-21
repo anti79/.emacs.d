2b6b3d0f536883e47337e8a5877bbe999e8b29b4
daniel <@noemail>
2024-10-21 12:22:29 +0300

    laptop

--- init.el
+++ init.el
@@ -4,27 +4,35 @@
 
 (setq initial-scratch-message "")
 (setq initial-major-mode 'text-mode)
-
-;; packages
+(auto-save-mode -1)
+(global-display-line-numbers-mode)
 (require 'package)
-
-(defmacro append-to-list (target suffix)
-  `(setq ,target (append ,target ,suffix)))
-(append-to-list package-archives
-                '(("melpa" . "http://melpa.org/packages/") ;; Main package archive
-                  ("melpa-stable" . "http://stable.melpa.org/packages/") 
-                  ("org-elpa" . "https://orgmode.org/elpa/")))
-
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(deeper-blue))
- '(package-selected-packages '(elpy))
- '(xterm-mouse-mode t))
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs flycheck company which-key dap-mode php-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+(which-key-mode)
+(add-hook 'php-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-php)
+  (yas-global-mode))
+
+(global-display-line-numbers-mode)
+(xterm-mouse-mode t)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,3 +52,16 @@
   :ensure t
   :init
   (elpy-enable))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages
+   '(dired-sidebar lsp-mode yasnippet lsp-treemacs flycheck company which-key dap-mode php-mode)))
+
+
+(use-package dired-sidebar
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar))
