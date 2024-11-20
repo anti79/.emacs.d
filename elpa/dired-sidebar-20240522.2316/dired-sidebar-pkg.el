3f5dcd0298cdc7416194e5cd5e7264e801e84c2f
daniel <@noemail>
2024-11-20 15:34:15 +0200

    packages?

---
+++ elpa/dired-sidebar-20240522.2316/dired-sidebar-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dired-sidebar" "20240522.2316"
+  "Tree browser leveraging dired."
+  '((emacs         "25.1")
+    (dired-subtree "0.0.1"))
+  :url "https://github.com/jojojames/dired-sidebar"
+  :commit "702165ad53a473992d84e0207b984b9be5114bde"
+  :revdesc "702165ad53a4"
+  :keywords '("dired" "files" "tools")
+  :authors '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers '(("James Nguyen" . "james@jojojames.com")))
